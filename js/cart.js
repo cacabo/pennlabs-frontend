6097ddbe0e9c9d8a18725a44fa6b77fba5727d10
ccabo1 <ccabo@seas.upenn.edu>
2017-09-16 20:33:02 -0400

    Smooth scroll

--- js/cart.js
+++ js/cart.js
@@ -46,6 +46,8 @@ function renderProducts() {
   $('#root').html(ans);
 }
 
+
+// Render the products on page load
 $(function() {
   renderProducts();
 });
@@ -53,18 +55,26 @@ $(function() {
 $(document).ready(function() {
   const $btn = $('#cart-button');
   const $cart = $('#cart');
+  const $icon = $('#cart-button .fa');
 
   $btn.click(function() {
     if ($cart.hasClass('show')) {
       $btn.removeClass('show');
       $cart.removeClass('show').hide(200);
-      $cart.css('color','white');
+      $cart.css('color', 'white');
     } else {
       $btn.addClass('show');
       $cart.addClass('show').show(200);
       setTimeout(function() {
-        $($cart).css('color','black');
+        $($cart).css('color', 'black');
       }, 200);
     }
+  });
+
+  $('.checkout').click(function() {
+    $(this).html('Mmmmaybe later &nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i>');
+    setTimeout(function() {
+      $('.checkout').html('Checkout &nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i>');
+    }, 600);
   });
 });
